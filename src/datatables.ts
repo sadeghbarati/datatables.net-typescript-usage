8660918fb93c789c4732402dbff3a9fc758b45ce
Sadegh Barati <sadeghbaratiwork@gmail.com>
2022-06-12 14:02:58 +0430

    first commit

---
+++ src/datatables.ts
@@ -0,0 +1,71 @@
+import jQuery from 'jquery'
+
+
+import dt from 'datatables.net'
+import dtbs5 from 'datatables.net-bs5'
+import AutoFill from 'datatables.net-autofill'
+import AutoFillBs5 from 'datatables.net-autofill-bs5'
+
+import Buttons from 'datatables.net-buttons'
+import ButtonsBs5 from 'datatables.net-buttons-bs5'
+import colVis from 'datatables.net-buttons/js/buttons.colVis'
+import html5 from 'datatables.net-buttons/js/buttons.html5'
+import print from 'datatables.net-buttons/js/buttons.print'
+import ColReorder from 'datatables.net-colreorder'
+import ColReorderBs5 from 'datatables.net-colreorder-bs5'
+import FixedColumns from 'datatables.net-fixedcolumns'
+import FixedColumnsBs5 from 'datatables.net-fixedcolumns-bs5'
+import FixedHeader from 'datatables.net-fixedheader'
+import FixedHeaderBs5 from 'datatables.net-fixedheader-bs5'
+import KeyTable from 'datatables.net-keytable'
+import KeyTableBs5 from 'datatables.net-keytable-bs5'
+import Responsive from 'datatables.net-responsive'
+import ResponsiveBs5 from 'datatables.net-responsive-bs5'
+import RowGroup from 'datatables.net-rowgroup'
+import RowGroupBs5 from 'datatables.net-rowgroup-bs5'
+import RowReorder from 'datatables.net-rowreorder'
+import RowReorderBs5 from 'datatables.net-rowreorder-bs5'
+import Scroller from 'datatables.net-scroller'
+import ScrollerBs5 from 'datatables.net-scroller-bs5'
+import SearchBuilder from 'datatables.net-searchbuilder'
+import SearchBuilderBs5 from 'datatables.net-searchbuilder-bs5'
+import SearchPanes from 'datatables.net-searchpanes'
+import SearchPanesBs5 from 'datatables.net-searchpanes-bs5'
+import Select from 'datatables.net-select'
+import SelectBs5 from 'datatables.net-select-bs5'
+import StateRestore from 'datatables.net-staterestore'
+import StateRestoreBs5 from 'datatables.net-staterestore-bs5'
+
+new dt() // has declaration
+new dtbs5() // has empty declaration
+new AutoFill() // has declaration
+new AutoFillBs5() // has empty declaration
+new Buttons() // has declaration
+new ButtonsBs5() // has empty declaration
+new colVis()
+new html5()
+new print()
+new ColReorder() // has declaration
+new ColReorderBs5() // has empty declaration
+// new FixedColumns() has no call signatures. has declaration
+new FixedColumnsBs5() // has empty declaration
+new FixedHeader() // has declaration
+new FixedHeaderBs5() // has empty declaration
+new KeyTable() // has declaration
+new KeyTableBs5() // has empty declaration
+new RowGroup() // has declaration
+new RowGroupBs5() // has empty declaration
+new RowReorder() // No declaration
+new RowReorderBs5() // No declaration
+new Responsive() // has declaration
+new ResponsiveBs5() // has empty declaration
+new Scroller() // has declaration
+new ScrollerBs5() // has empty declaration
+// new SearchBuilder() has no call signatures. has declaration
+new SearchBuilderBs5() // has empty declaration
+// new SearchPanes() has no call signatures. has declaration
+new SearchPanesBs5() // has empty declaration
+new Select() // has declaration
+new SelectBs5() // has empty declaration
+// new StateRestore() has no call signatures. has declaration
+new StateRestoreBs5() // has empty declaration
